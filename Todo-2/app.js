f72678ecb2d79e3a6030d9fe1505b147823f6e53
lambert34 <aleksandrlambert@gmail.com>
2023-03-27 15:23:00 +0700

    правки

--- Todo-2/app.js
+++ Todo-2/app.js
@@ -22,3 +22,32 @@ form.onsubmit = function(evt) {
     };
     list.append(newElement);
 }
+
+priority.onclick = function () {
+    priority.classList.toggle('is-important');
+    if (priority.classList.contains('is-important')) {
+        priority.textContent = 'Важная задача';
+    } else {
+        priority.textContent = 'Обычная задача'
+    }
+};
+
+priority.onclick = function () {
+    priority.classList.toggle('is-important');
+    if (priority.classList.contains('is-important')) {
+        priority.textContent = 'Важная задача';
+    } else {
+        priority.textContent = 'Обычная задача'
+    }
+};
+
+form.onsubmit = function(evt) {
+    evt.preventDeafault();
+    let newElement = document.createElement('li');
+    newElement.textContent = input.value;
+    input.value = '';
+    if (priority.classList.contains('is-important')) {
+        newElement.classList.add('is-important');
+    };
+    list.append(newElement);
+}
